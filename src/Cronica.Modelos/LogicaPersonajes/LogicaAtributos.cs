df966e00646f4c3b293061343389d97bd28849e2
herruzojm <herruzojm@gmail.com>
2016-01-18 17:28:57 +0100

    Correccion vistas

--- src/Cronica.Modelos/LogicaPersonajes/LogicaAtributos.cs
+++ src/Cronica.Modelos/LogicaPersonajes/LogicaAtributos.cs
@@ -1,11 +1,8 @@
 ﻿using Cronica.Modelos.ViewModels.Trama;
 using Cronica.Models;
 using Cronica.ViewModels.Personaje;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Transactions;
 
 namespace Cronica.Modelos.LogicaPersonajes
 {
@@ -20,43 +17,26 @@ namespace Cronica.Modelos.LogicaPersonajes
 
         public async Task<int> CrearAtributo(Atributo atributo)
         {
-            int resultado;
             _contexto.Atributos.Add(atributo);
-            using (TransactionScope transaccion = new TransactionScope())
-            {
-                resultado = await _contexto.SaveChangesAsync();
-                if (resultado > 0)
-                {
-                    resultado = await IncluirAtributoEnPersonajes(atributo);
-                }
-                if (resultado > 0)
-                {
-                    resultado = await IncluirAtributoEnPlantillasTrama(atributo);
-                }
-                if (resultado > 0)
-                {
-                    transaccion.Complete();
-                }
-            }           
-            return resultado;
+            IncluirAtributoEnPersonajes(atributo);
+            IncluirAtributoEnPlantillasTrama(atributo);
+            return await _contexto.SaveChangesAsync();                
         }
         
-        private async Task<int> IncluirAtributoEnPersonajes(Atributo nuevoAtributo)
+        private void IncluirAtributoEnPersonajes(Atributo nuevoAtributo)
         {
             var personajes = _contexto.Personajes.ToList<Personaje>();
             personajes.ForEach(p => p.Atributos.Add(
                     new AtributoPersonaje() { AtributoId = nuevoAtributo.AtributoId, PersonajeId = p.PersonajeId, Valor = 0 }
-                ));
-            return await _contexto.SaveChangesAsync();
+                ));            
         }
 
-        private async Task<int> IncluirAtributoEnPlantillasTrama(Atributo nuevoAtributo)
+        private void IncluirAtributoEnPlantillasTrama(Atributo nuevoAtributo)
         {
             var plantillasTrama = _contexto.PlantillasTrama.ToList();
             plantillasTrama.ForEach(p => p.Atributos.Add(
                     new AtributoPlantillaTrama() { AtributoId = nuevoAtributo.AtributoId, PlantillaTramaId = p.PlantillaTramaId, Multiplicador = 0 }
-                ));
-            return await _contexto.SaveChangesAsync();
+                ));            
         }
 
     }
