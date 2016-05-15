fccb6940756eae9269226688e5250741ef365c67
Herruzojm <herruzojm@gmail.com>
2016-05-15 15:30:21 +0200

    Menu de usuario lateral por tipo de cuenta

---
+++ src/Cronica/ViewComponents/MenuUsuario.cs
@@ -0,0 +1,27 @@
+﻿using Cronica.Modelos.Models;
+using Cronica.Servicios.Interfaces;
+using Microsoft.AspNet.Mvc;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace Cronica.ViewComponents
+{
+    public class MenuUsuario : ViewComponent
+    {
+        private IServicioUsuarios _servicioUsuarios;
+
+        public MenuUsuario(IServicioUsuarios servicioUsuarios)
+        {
+            _servicioUsuarios = servicioUsuarios;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string userId)
+        {
+            ApplicationUser usuario = await _servicioUsuarios.GetUsuarioById(userId);            
+            return View(usuario);
+        }
+
+    }
+}
